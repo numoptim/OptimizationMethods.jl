001af85d8e89043e8f7a3b273323eb01ba8be539
Christian Varner <varner.christian.a@gmail.com>
2024-09-25 10:36:50 -0500

    runtest script

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,15 @@
-using OptimizationMethods
+# Date: 09/25/2025
+# Author: Christian Varner
+# Purpose: Run tests
+
 using Test
 
-@testset "OptimizationMethods.jl" begin
-    # Write your tests here.
+@testset verbose=true "OptimizationMethods.jl" begin
+
+    getTests = `./test/getTests.sh`
+    run(getTests);
+
+    for file in readlines(joinpath(@__DIR__, "test.txt"))
+        include(file)
+    end
 end
