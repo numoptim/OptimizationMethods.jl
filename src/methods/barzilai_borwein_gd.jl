e64f33773ed7907b34d3a401d82c6c21870f78b7
Vivak Patel <vp314@users.noreply.github.com>
2024-11-02 12:13:20 -0400

    Changed gra_val_hist to grad_val_hist to keep it consistent with NLPModel api terminology

--- src/methods/barzilai_borwein_gd.jl
+++ src/methods/barzilai_borwein_gd.jl
@@ -19,7 +19,7 @@ A structure for storing data about gradient descent using the Barzilai-Borwein
 - `iter_hist::Vector{Vector{T}}`, a history of the iterates. The first entry
     corresponds to the initial iterate (i.e., at iteration `0`). The `k+1` entry
     corresponds to the iterate at iteration `k`.
-- `gra_val_hist::Vector{T}`, a vector for storing `max_iterations+1` gradient
+- `grad_val_hist::Vector{T}`, a vector for storing `max_iterations+1` gradient
     norm values. The first entry corresponds to iteration `0`. The `k+1` entry
     correpsonds to the gradient norm at iteration `k`.
 - `stop_iteration::Int64`, the iteration number that the solver stopped on.
@@ -55,7 +55,7 @@ mutable struct BarzilaiBorweinGD{T} <: AbstractOptimizerData{T}
     threshold::T
     max_iterations::Int64
     iter_hist::Vector{Vector{T}}
-    gra_val_hist::Vector{T}
+    grad_val_hist::Vector{T}
     stop_iteration::Int64
 end
 function BarzilaiBorweinGD(
@@ -72,12 +72,12 @@ function BarzilaiBorweinGD(
     iter_hist = Vector{T}[Vector{T}(undef, d) for i in 1:max_iterations + 1]
     iter_hist[1] = x0
 
-    gra_val_hist = Vector{T}(undef, max_iterations + 1)
+    grad_val_hist = Vector{T}(undef, max_iterations + 1)
     stop_iteration = -1
 
     return BarzilaiBorweinGD("Gradient Descent with Barzilai-Borwein Step Size", 
         init_stepsize, long_stepsize, threshold, max_iterations, 
-        iter_hist, gra_val_hist, stop_iteration)
+        iter_hist, grad_val_hist, stop_iteration)
 end
 
 """
@@ -154,7 +154,7 @@ function barzilai_borwein_gd(
 
     # save initial values 
     OptimizationMethods.grad!(progData, precomp, store, x)
-    optData.gra_val_hist[iter + 1] = norm(store.grad)
+    optData.grad_val_hist[iter + 1] = norm(store.grad)
 
     # first step
     iter += 1
@@ -163,10 +163,10 @@ function barzilai_borwein_gd(
 
     OptimizationMethods.grad!(progData, precomp, store, x)
     optData.iter_hist[iter + 1] .= x
-    optData.gra_val_hist[iter + 1] = norm(store.grad)
+    optData.grad_val_hist[iter + 1] = norm(store.grad)
 
     # main iteration
-    while (iter < optData.max_iterations) && (optData.gra_val_hist[iter + 1] > optData.threshold)
+    while (iter < optData.max_iterations) && (optData.grad_val_hist[iter + 1] > optData.threshold)
         iter += 1
 
         # compute Δx and Δg for the step size using memory already allocated
@@ -181,7 +181,7 @@ function barzilai_borwein_gd(
         # save values
         OptimizationMethods.grad!(progData, precomp, store, x)
         optData.iter_hist[iter + 1] .= x
-        optData.gra_val_hist[iter + 1] = norm(store.grad) 
+        optData.grad_val_hist[iter + 1] = norm(store.grad) 
     end
     optData.stop_iteration = iter
 
