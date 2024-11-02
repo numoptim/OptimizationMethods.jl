f383274463b4f77dc21e84d2c58563eb37a4d4ee
Vivak Patel <vp314@users.noreply.github.com>
2024-11-02 08:53:37 -0400

    Renamed initial and long step size boolean to be interpretable; added mathematical description to docstring

--- src/methods/barzilai_borwein_gd.jl
+++ src/methods/barzilai_borwein_gd.jl
@@ -1,20 +1,21 @@
-# Date: 09/16/2024
-# Author: Christian Varner
-# Purpose: Implement barzilai-borwein.
+# OptimizationMethods.jl
 
 """
     BarzilaiBorweinGD{T} <: AbstractOptimizerData{T}
 
-A structure for storing data about gradient descent using the Barzilai-Borwein step size,
-    and the progress of its application on an optimization problem.
+A structure for storing data about gradient descent using the Barzilai-Borwein 
+    step size, and the progress of its application on an optimization problem.
 
 # Fields
 
 - `name:String`, name of the solver for reference.
-- `alfa0::T`, initial step size to start the method.
-- `long::Bool`, flag for step size; if true, use the long version of Barzilai-Borwein, if false, use the short version.
-- `threshold::T`, gradient threshold. If the norm gradient is below this, then iteration stops.
-- `max_iterations::Int64`, max number of iterations (gradient steps) taken by the solver.
+- `init_stepsize::T`, initial step size to start the method. 
+- `long_stepsize::Bool`, flag for step size; if true, use the long version of 
+    Barzilai-Borwein. If false, use the short version. 
+- `threshold::T`, gradient threshold. If the norm gradient is below this, then 
+    iteration stops.
+- `max_iterations::Int64`, max number of iterations (gradient steps) taken by 
+    the solver.
 - `iter_hist::Vector{Vector{T}}`, a history of the iterates. The first entry
     corresponds to the initial iterate (i.e., at iteration `0`). The `k+1` entry
     corresponds to the iterate at iteration `k`.
@@ -26,8 +27,10 @@ A structure for storing data about gradient descent using the Barzilai-Borwein s
 
 # Constructors
 
-    BarzilaiBorweinGD(::Type{T}; x0::Vector{T}, alfa0::T, long::Bool, threshold::T,
-    max_iterations::Int) where {T}
+    BarzilaiBorweinGD(::Type{T}; x0::Vector{T}, init_stepsize::T, 
+        long_stepsize::Bool, threshold::T, max_iterations::Int) where {T}
+
+Constructs the `struct` for the Barzilai-Borwein optimization method
 
 ## Arguments
 
@@ -36,16 +39,19 @@ A structure for storing data about gradient descent using the Barzilai-Borwein s
 ## Keyword Arguments
 
 - `x0::Vector{T}`, initial point to start the solver at.
-- `alfa0::T`, initial step size used for the first iteration.
-- `long::Bool`, flag for step size; if true, use the long version of Barzilai-Borwein, if false, use the short version. 
-- `threshold::T`, gradient threshold. If the norm gradient is below this, then iteration is terminated. 
-- `max_iterations::Int`, max number of iterations (gradient steps) taken by the solver.
+- `init_stepsize::T`, initial step size used for the first iteration. 
+- `long_stepsize::Bool`, flag for step size; if true, use the long version of
+    Barzilai-Borwein, if false, use the short version. 
+- `threshold::T`, gradient threshold. If the norm gradient is below this, 
+    then iteration is terminated. 
+- `max_iterations::Int`, max number of iterations (gradient steps) taken by 
+    the solver.
 
 """
 mutable struct BarzilaiBorweinGD{T} <: AbstractOptimizerData{T}
     name::String
-    alfa0::T
-    long::Bool
+    init_stepsize::T 
+    long_stepsize::Bool 
     threshold::T
     max_iterations::Int64
     iter_hist::Vector{Vector{T}}
@@ -55,8 +61,8 @@ end
 function BarzilaiBorweinGD(
     ::Type{T};
     x0::Vector{T},
-    alfa0::T,
-    long::Bool,
+    init_stepsize::T, 
+    long_stepsize::Bool, 
     threshold::T,
     max_iterations::Int,
 ) where {T}
@@ -69,23 +75,47 @@ function BarzilaiBorweinGD(
     gra_val_hist = Vector{T}(undef, max_iterations + 1)
     stop_iteration = -1
 
-    return BarzilaiBorweinGD("Gradient Descent with Barzilai-Borwein", alfa0, long, threshold, max_iterations, iter_hist, gra_val_hist,
-    stop_iteration)
+    return BarzilaiBorweinGD("Gradient Descent with Barzilai-Borwein Step Size", 
+        init_stepsize, long_stepsize, threshold, max_iterations, 
+        iter_hist, gra_val_hist, stop_iteration)
 end
 
 """
 
-    barzilai_borwein_gd(optData::BarzilaiBorweinGD{T}, progData::P where P <: AbstractNLPModel{T, S}) 
-        where {T,S} 
+    barzilai_borwein_gd(optData::BarzilaiBorweinGD{T}, progData::P 
+        where P <: AbstractNLPModel{T, S}) where {T,S} 
 
-Implements gradient descent with Barzilai-Borwein step size
-and applies the method to the optimization problem specified by `progData`. 
-To see more about the method, take a look at:
+Implements gradient descent with Barzilai-Borwein step size and applies the 
+    method to the optimization problem specified by `progData`. 
 
-Barzilai and Borwein. "Two-Point Step Size Gradient Methods". IMA Journal of Numerical Analysis.
+# Reference(s)
 
-The method will take advantage of precomputed values and allocated space initialized 
-by calling `initialize(progData)` (see documentation for problems).
+Barzilai and Borwein. "Two-Point Step Size Gradient Methods". IMA Journal of 
+    Numerical Analysis.
+
+# Method
+
+Given iterates ``\\lbrace x_0,\\ldots,x_k\\rbrace``, the iterate ``x_{k+1}``
+    is equal to ``x_k - \\alpha_k \\nabla f(x_k)``, where ``\\alpha_k`` is
+    one of two versions.
+
+## Long Step Size Version (if `optData.long_stepsize==true`)
+
+If ``k=0``, then ``\\alpha_0`` is set to `optData.init_stepsize`. For ``k>0``,
+
+```math 
+\\alpha_k = \\frac{ \\Vert x_k - x_{k-1} \\Vert_2^2}{(x_k - x_{k-1}^\\intercal 
+    (\\nabla f(x_k) - \\nabla f(x_{k-1})))}.
+```
+
+## Short Step Size Version (if `optData.long_stepsize==false`)
+
+If ``k=0``, then ``\\alpha_0`` is set to `optData.init_stepsize`. For ``k>0``,
+
+```math
+\\alpha_k = \\frac{(x_k - x_{k-1}^\\intercal (\\nabla f(x_k) - 
+    \\nabla f(x_{k-1})))}{\\Vert \\nabla f(x_k) - \\nabla f(x_{k-1})\\Vert_2^2}.
+```
 
 # Arguments
 
@@ -116,7 +146,7 @@ function barzilai_borwein_gd(
     # initialization
     iter = 0
     precomp, store = OptimizationMethods.initialize(progData) 
-    step_size = optData.long ? _long_step_size : _short_step_size
+    step_size = optData.long_stepsize ? _long_step_size : _short_step_size
     x = copy(optData.iter_hist[iter + 1]) 
 
     # buffer for previous gradient value
@@ -128,7 +158,7 @@ function barzilai_borwein_gd(
 
     # first step
     iter += 1
-    x .-= optData.alfa0 .* store.grad
+    x .-= optData.init_stepsize .* store.grad
     gprev .= store.grad
 
     OptimizationMethods.grad!(progData, precomp, store, x)
