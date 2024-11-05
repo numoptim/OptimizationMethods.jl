5a657eab8947511e2be8860d92cd4eb4e8d3abdc
Vivak Patel <vp314@users.noreply.github.com>
2024-11-05 09:47:31 -0600

    Added more unit tests

--- test/methods/gd_barzilai_borwein.jl
+++ test/methods/gd_barzilai_borwein.jl
@@ -40,31 +40,35 @@ using Test, OptimizationMethods, LinearAlgebra, Random
     real_types = [Int16, Int32, Int64, Float16, Float32, Float64]
     param_dim = 10
 
-    for real_type in real_types
+    for bool in [false, true]
+        for real_type in real_types
 
-        field_types = [String, real_type, Bool, real_type, Int64, 
-            Vector{real_type}, Vector{real_type}, Vector{Vector{real_type}},
-            Vector{real_type}, Int64]
+            field_types = [String, real_type, Bool, real_type, Int64, 
+                Vector{real_type}, Vector{real_type}, Vector{Vector{real_type}},
+                Vector{real_type}, Int64]
 
-        bb_method = BarzilaiBorweinGD(real_type,
-            x0 = zeros(real_type, param_dim),
-            init_stepsize = real_type(1),
-            long_stepsize = true,
-            threshold = real_type(100.0),
-            max_iterations = 100)
-        
-        for (field_name, field_type) in zip(names, field_types)
-            @test typeof(getfield(bb_method, field_name)) == field_type
+            bb_method = BarzilaiBorweinGD(real_type,
+                x0 = zeros(real_type, param_dim),
+                init_stepsize = real_type(1),
+                long_stepsize = bool,
+                threshold = real_type(100.0),
+                max_iterations = 100)
+            
+            for (field_name, field_type) in zip(names, field_types)
+                @test typeof(getfield(bb_method, field_name)) == field_type
+            end
         end
     end
 
     ## test assertions
-    @test_throws AssertionError BarzilaiBorweinGD(Float64, 
-        x0=randn(10), 
-        init_stepsize = -0.3, #Assertion error, must be positive
-        long_stepsize = true, 
-        threshold = 1e-10, 
-        max_iterations = 100)
+    for bool in [false, true]
+        @test_throws AssertionError BarzilaiBorweinGD(Float64, 
+            x0=randn(10), 
+            init_stepsize = -0.3, #Assertion error, must be positive
+            long_stepsize = bool, 
+            threshold = 1e-10, 
+            max_iterations = 100)
+    end
 
     ##########################################
     # Test optimizer
@@ -104,7 +108,45 @@ using Test, OptimizationMethods, LinearAlgebra, Random
         @test optData.stop_iteration == 1
     end
 
-    ## Many Step Updates 
+    ## Two Step Update 
+    for bool in [false, true]
+        optData = BarzilaiBorweinGD(
+            Float64,
+            x0 = x0,
+            init_stepsize = ss,
+            long_stepsize = bool,
+            threshold = 1e-10,
+            max_iterations = 2
+        )
+
+        # Output after two steps
+        x2 = barzilai_borwein_gd(optData, progData)
+        x1 = optData.iter_hist[2]
+
+        # Test stored values 
+        g2 = OptimizationMethods.grad(progData, x2)
+        g1 = OptimizationMethods.grad(progData, x1)
+        g0 = OptimizationMethods.grad(progData, x0)
+        @test optData.iter_hist[1] == x0 
+        @test optData.iter_hist[3] == x2
+        @test optData.iter_diff ≈ x2 - x1
+        @test optData.grad_diff ≈ g2 - g1
+        @test optData.grad_val_hist[1] ≈ norm(g0)
+        @test optData.grad_val_hist[2] ≈ norm(g1)
+        @test optData.grad_val_hist[3] ≈ norm(g2)
+        @test optData.stop_iteration == 2
+
+        # Test Step Size
+        if bool #true for long_stepsize
+            long_step_size = norm(x1 - x0)^2 / dot(x1 - x0, g1 - g0)
+            @test x2 ≈ x1 - long_step_size * g1 
+        else
+            short_step_size = dot(x1 - x0, g1 - g0) / norm(g1 - g0)^2
+            @test x2 ≈ x1 - short_step_size * g1
+        end
+    end
+
+    ## Induction Step Updates 
     for bool in [false, true]
         optData = BarzilaiBorweinGD(
             Float64, 
@@ -117,6 +159,22 @@ using Test, OptimizationMethods, LinearAlgebra, Random
 
         # Output
         xk = barzilai_borwein_gd(optData, progData)
+        xk1 = optData.iter_hist[optData.stop_iteration-1]
+        xk2 = optData.iter_hist[optData.stop_iteration-2]
+
+        gk = OptimizationMethods.grad(progData, xk)
+        gk1 = OptimizationMethods.grad(progData, xk1)
+        gk2 = OptimizationMethods.grad(progData, xk2)
+
+        #Test Stored Values k-1 to k+1
+        @test optData.iter_hist[optData.stop_iteration] ≈ xk
+        @test optData.iter_diff ≈ xk - xk1 atol=1e-4
+        @test optData.grad_diff ≈ gk - gk1 atol=1e-4
+        @test optData.grad_val_hist[optData.stop_iteration] ≈ norm(gk) atol=1e-6
+        @test optData.grad_val_hist[optData.stop_iteration-1] ≈ 
+            norm(gk1) atol=1e-6
+        @test optData.grad_val_hist[optData.stop_iteration-2] ≈ 
+            norm(gk2) atol=1e-6
         
         # Stop Iteration should be 26
         if optData.stop_iteration < optData.max_iterations 
@@ -124,6 +182,15 @@ using Test, OptimizationMethods, LinearAlgebra, Random
                 optData.threshold
             @test optData.grad_val_hist[optData.stop_iteration + 1] < 
                 optData.threshold 
+        end
+
+        # Test Step Size
+        if bool
+            long_step_size = norm(xk1 - xk2)^2 / dot(xk1 - xk2, gk1 - gk2)
+            @test xk ≈ xk1 - long_step_size * gk1 
+        else
+            short_step_size = dot(xk1 - xk2, gk1 - gk2) / norm(gk1 - gk2)^2
+            @test xk ≈ xk1 - short_step_size * gk1
         end
     end
     
