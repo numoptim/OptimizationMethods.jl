eca4553a44a16b5f2b189de23e3e33672affdf22
Christian Varner <varner.christian.a@gmail.com>
2025-01-23 07:37:26 -0600

    examples using negative curvature directions

---
+++ examples/prob-LR_solve-NegCurv.jl
@@ -0,0 +1,44 @@
+using OptimizationMethods
+
+progData = OptimizationMethods.LogisticRegression(Float64)
+optData = NegativeCurvatureGD(
+    Float64;
+    x0 = randn(50),
+    alpha = 1e-3,
+    beta = 1e-3,
+    threshold = 1e-10,
+    max_iterations = 1000
+)
+
+x = negative_curvature_gd(optData, progData)
+
+# Compute objective and residual evals during optimization 
+obj_evals = progData.counters.neval_obj
+
+# Compute objective values of different iterates for reporting purposes
+obj_init = OptimizationMethods.obj(progData, optData.iter_hist[1])
+obj_term = OptimizationMethods.obj(progData, 
+    optData.iter_hist[optData.stop_iteration + 1])
+
+
+println(
+"""
+    Problem: $(progData.meta.name)
+    Solver: $(optData.name)
+    Parameter Dimension: $(progData.meta.nvar)
+    
+    Max Iterations Allowed: $(optData.max_iterations)
+    Gradient Stopping Threshold: $(optData.threshold)
+
+    Initial Objective: $obj_init
+    Initial Grad Norm: $(optData.grad_val_hist[1])
+
+    Terminal Iteration: $(optData.stop_iteration)
+    Terminal Objective: $obj_term
+    Terminal Grad Norm: $(optData.grad_val_hist[optData.stop_iteration + 1])
+
+    Objective Evaluations: $obj_evals
+    Gradient Evaluations: $(progData.counters.neval_grad)
+    Hessian Evaluations: $(progData.counters.neval_hess)
+"""
+)
