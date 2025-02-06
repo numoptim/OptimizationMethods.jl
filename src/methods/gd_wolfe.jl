dbc4f796a8443016b51c1389bde3b1ab51ca0b0a
Christian Varner <varner.christian.a@gmail.com>
2025-02-06 09:54:24 -0600

    struct fields + rough draft documentation

--- src/methods/gd_wolfe.jl
+++ src/methods/gd_wolfe.jl
@@ -4,24 +4,65 @@
 # version of Wolfe line search
 
 """
-    TODO
+    WolfeGD{T} <: AbstractOptimizerData{T}
+
+Mutable `struct` that represents gradient descent using the ELBS routine to
+satisfy weak wolfe conditions. 
+
+# Fields
+
+# Constructors
+
+## Arguments
+
+## Keyword Arguments
 """
-mutable struct WolfeGD{T} <: AbstractOptimizerData{T}
+mutable struct WolfeEBLSGD{T} <: AbstractOptimizerData{T}
+    name::String
+    α::T
+    δ::T
+    c1::T
+    c2::T
+    line_search_max_iterations::Int
+    threshold::T
+    max_iterations::Int64
+    iter_hist::Vector{Vector{T}}
+    grad_val_hist::Vector{T}
+    stop_iteration::Int64
 end
-function WolfeGD(
+function WolfeEBLSGD(
     ::Type{T};
     x0::Vector{T},
-    # TODO - algorithm specific parameters
+    α::T,
+    δ::T,
+    c1::T,
+    c2::T, 
+    line_search_max_iterations::Int,
     threshold::T,
     max_iterations::Int
-)
+) where {T}
+
+    # name of the optimizer
+    name = "Gradient Descent with Line Search for Weak Wolfe Conditions"
+
+    # initialize iter_hist and grad_val_hist
+    d::Int64 = length(x0)
+    iter_hist::Vector{Vector{T}} = 
+        Vector{Vector{T}}([Vector{T}(undef, d) for i in 1:(max_iterations + 1)])
+    iter_hist[1] = x0
+
+    grad_val_hist::Vector{T} = Vector{T}(undef, max_iterations + 1) 
+    stop_iteration::Int64 = -1 ## dummy value
+
+    return WolfeEBLSGD(name, α, δ, c1, c2, line_search_max_iterations, threshold,
+        max_iterations, iter_hist, grad_val_hist, stop_iteration)
 end
 
 """
     TODO
 """
 function wolfe_gd(
-    optData::WolfeGD{T},
+    optData::WolfeEBLSGD{T},
     progData::P where P <: AbstractNLPModel{T, S}
 )
 end
