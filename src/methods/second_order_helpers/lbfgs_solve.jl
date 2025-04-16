6a03a9fa288a0491a1911af4e3d767fe744448f8
Christian Varner <varner.christian.a@gmail.com>
2025-04-16 12:35:28 -0500

    rough draft of implementation

---
+++ src/methods/second_order_helpers/lbfgs_solve.jl
@@ -0,0 +1,15 @@
+# Date: 2025/04/16
+# Author: Christian Varner
+# Purpose: Implementation of producing B_k * \dot F_k where
+# B_k is the inverse of the LBFGS matrix
+
+"""
+"""
+function lbfgs_solve!(
+    d::Vector{T}, 
+    s::Vector{Vector{T}}, 
+    y::Vector{Vector{T}},
+    ρ::Vector{T}) where {T}
+
+    
+end
