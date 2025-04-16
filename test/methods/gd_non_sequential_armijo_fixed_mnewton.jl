d95a66318833468e45c4f84c8692bb0b83fc9488
Christian Varner <varner.christian.a@gmail.com>
2025-04-16 14:06:19 -0500

    test case file

---
+++ test/methods/gd_non_sequential_armijo_fixed_mnewton.jl
@@ -0,0 +1,25 @@
+# Date: 2025/04/16
+# Author: Christian Varner
+# Purpose: Test the non-sequential armijo modified newton method with
+# fixed step size
+
+module TestNonsequentialArmijoFixedMNewtonGD
+
+using Test, OptimizationMethods, LinearAlgebra, CircularArrays
+
+@testset "Test NonsequentialArmijoFixedMNewtonGD Structure" begin
+end # end the structure tests
+
+@testset "Utility -- Update Algorithm Parameters Nonsequential Modified Newton" begin
+end # end the update parameter tests
+
+@testset "Utility -- Inner Loop Nonsequential Armijo Modified Newton" begin 
+end # end the test of the inner loop
+
+@testset "Method -- Nonsequential Armijo with Modified Newton (Monotone)" begin
+end # end the test for the method 
+
+@testset "Method -- Nonsequential Armijo with Modified Newton (Non-monotone)" begin
+end # end the test for the method 
+
+end # end the tests for NonsequentialArmijoFixedMNewtonGD
