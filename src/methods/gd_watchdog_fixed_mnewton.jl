debb55a646d805f9b9b65f544450e43ed20b5779
Christian Varner <varner.christian.a@gmail.com>
2025-05-07 08:12:21 -0500

    documentation for the inner loop function of watchdog modified newton

--- src/methods/gd_watchdog_fixed_mnewton.jl
+++ src/methods/gd_watchdog_fixed_mnewton.jl
@@ -177,6 +177,68 @@ function WatchdogFixedMNewtonGD(::Type{T};
 end
 
 """
+    inner_loop!(ψjk::S, θk::S, optData::WatchdogFixedMNewtonGD{T}, 
+        progData::P1 where P1 <: AbstractNLPModel{T, S}, 
+        precomp::P2 where P2 <: AbstractPrecompute{T}, 
+        store::P3 where P3 <: AbstractProblemAllocate{T}, 
+        k::Int64; max_iterations = 100) where {T}
+
+Conduct the inner loop iteration, modifying `ψjk`, `optData`, and `store` in
+    place. `ψjk` gets updated to be the terminal iterate of the inner loop.
+    This inner loop function uses fixed step sizes with modified Newton
+    directions.
+
+# Method
+
+In what follows, we let ``||\\cdot||_2`` denote the L2-norm. 
+Let ``\\theta_{k}`` for ``k + 1 \\in\\mathbb{N}`` be the ``k^{th}`` iterate
+of the optimization algorithm. Let ``\\alpha =`` `optData.α`.
+
+Let ``\\psi_0^k = \\theta_k``, then this method returns
+```math
+    \\psi_{j_k}^k = \\psi_0^k - \\sum_{i = 0}^{j_k-1} \\alpha_j^k d_i^k,
+```
+where ``j_k \\in \\mathbb{N}`` is the smallest iteration for which 
+at least one of the conditions are satisfied: 
+
+1. ``j_k == `` `optData.inner_loop_max_iterations`
+2. ``||\\dot F(\\psi_{j_k}^k)||_2 \\leq \\eta (1 + |F(\\theta_k)|)`` and
+    ``|F(\\psi_{j_k}^k| \\leq `` `optData.reference_value`.
+
+The step direction ``d_i^k`` is one of the following. Let ``\\ddot F(\\psi_i^k)``
+be the hessian matrix of ``F`` at ``\\psi_i^k`` for ``i + 1 \\in \\mathbb{N}``
+and ``k + 1 \\in \\mathbb{N}``. Then, if 
+[OptimizationMethods.add_identity_until_pd!](@ref) successful modifies
+``\\ddot F(\\psi_i^k)``, returning ``H_i^k``, then
+
+```math
+    d_i^k = (H_i^k)^{-1} \\dot F(\\psi_i^k).
+```
+
+Otherwise, if this routine is not successful ``d_i^k = \\dot F(\\psi_i^k)``.
+
+# Arguments
+
+- `ψjk::S`, buffer array for the inner loop iterates.
+- `θk::S`, starting iterate.
+- `optData::WatchdogFixedMNewtonGD{T}`, `struct` that specifies the optimization
+    algorithm. Fields are modified during the inner loop.
+- `progData::P1 where P1 <: AbstractNLPModel{T, S}`, `struct` that specifies the
+    optimization problem. Fields are modified during the inner loop.
+- `precomp::P2 where P2 <: AbstractPrecompute{T}`, `struct` that has precomputed
+    values. Required to take advantage of this during the gradient computation.
+- `store::P3 where P3 <: AbstractProblemAllocate{T}`, `struct` that contains
+    buffer arrays for computation.
+- `k::Int64`, outer loop iteration for computation of the local Lipschitz
+    approximation scheme.
+
+## Optional Keyword Arguments
+
+- `max_iteration = 100`, maximum number of allowable iteration of the inner loop.
+
+# Returns
+
+- `j::Int64`, the iteration for which a triggering event evaluated to true.
 """
 function inner_loop!(
     ψjk::S,
