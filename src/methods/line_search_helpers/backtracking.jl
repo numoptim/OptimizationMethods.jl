01c5111e4e7b76bf29bf1a0e9bbfe8c56d6dba03
Christian Varner <varner.christian.a@gmail.com>
2025-07-08 05:07:48 -0500

    forgot to save file

--- src/methods/line_search_helpers/backtracking.jl
+++ src/methods/line_search_helpers/backtracking.jl
@@ -48,11 +48,7 @@ where ``O_{k-1}`` is some reference value.
     Larger values correspond to stricter descent conditions, and
     smaller values correspond to looser descent conditions on `θk`.
 
-<<<<<<< HEAD
-## Optional Keyword Arguments
-=======
 # Optional Keyword Arguments
->>>>>>> main-v0.0.1
 
 - `max_iteration::Int64 = 100`, the maximum allowable iterations
     for the line search procedure. In the language above, when
